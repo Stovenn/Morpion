ca6167e9751fabc24ebf0de78de874d8edef9842
Stovenn <jonathan.sylvestre.pro@gmail.com>
2020-01-31 00:01:03 +0100

    petite correction

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ require_relative 'lib/game.rb'
 require_relative 'lib/player.rb'
 require_relative 'lib/board.rb'
 require_relative 'lib/board_case.rb'
-require_relative 'lib/application.rb'
+
 
 
 new_game = Game.new
@@ -14,4 +14,3 @@ while new_game.status == "on going"
 end
 new_game.new_round
 
-# binding.pry
